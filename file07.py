fce28c02398063e0882112bf764e5a2b46c865e1
asrorc1k <asrornortojiev@gmail.com>
2022-11-02 12:18:33 +0300

    07

--- file07.py
+++ file07.py
@@ -8,3 +8,11 @@ def main(data:str):
     """
     
 # Read data from file
+a=0
+    for i in f:
+        if i in '0123456789':
+            a+=int(i)
+    return a
+f = open('txt_file/data07.txt')
+f = f.read()
+print(main(f))
