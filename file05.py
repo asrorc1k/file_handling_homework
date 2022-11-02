4248d5f1b26408836cd008853a226cbfe2eb9236
asrorc1k <asrornortojiev@gmail.com>
2022-11-02 12:16:49 +0300

    05

--- file05.py
+++ file05.py
@@ -8,3 +8,12 @@ def main(data:str):
     """
     
 # Read data from file
+i=0
+    k=0
+    for i in f:
+        if i in ('0123456789'):
+            k+=1
+        answer = k , len(f)-k
+    return list(answer)
+f = open('txt_file/data05.txt').read()
+print(main(f))
